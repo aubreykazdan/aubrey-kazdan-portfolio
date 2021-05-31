3828759eb64eecef61c22cb76c3672bc2829366a
Aubrey Kazdan <aubrey.j.kazdan@gmail.com>
2021-05-31 17:41:07 -0400

    ham nav button works

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
-// $(window).scroll(function() {
-//     if ($(".navbar").offset().top > 50) {
-//         $(".navbar-fixed-top").addClass("top-nav-collapse");
-//     } else {
-//         $(".navbar-fixed-top").removeClass("top-nav-collapse");
-//     }
-// });
+// NAVBAR FUNCTION
 
-const navBarButton = document.querySelector('.navbar-toggle');
+const ham = document.querySelector('.navbarToggle');
+const menu = document.querySelector('.navList')
 
-const navBarFixed = document.querySelector('.navbar-fixed-top');
-console.log(navBarFixed);
-
-navBarButton.addEventListener('click', console.log('button has been clicked'));
-
-navBarButton.addEventListener('click', function() {
-    if (navBarButton.classList === 'collapsed') {
-        navBarButton.classList.remove('collapsed')
-        console.log('collapsed has been removed')
-    } else if (navBarButton.classList != 'collapsed') {
-        navBarButton.classList.add('collapsed');
-        console.log('collapsed has been added');
+// This is to unhide menu
+ham.addEventListener('click', toggleMenu);
+function toggleMenu() {
+    if (menu.classList.contains("showMenu")) {
+        menu.classList.remove("showMenu");
+    } else {
+        menu.classList.add("showMenu");
     }
-})
+}
+
+const menuLinks = document.querySelectorAll(".menuLink")
+
+menuLinks.forEach (
+    function(menuLink) {
+        menuLink.addEventListener('click', toggleMenu);
+    }
+)
