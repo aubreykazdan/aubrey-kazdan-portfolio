9124c0fd51b5d212cbb0048458acfa8a98d7a42e
Aubrey Kazdan <aubrey.j.kazdan@gmail.com>
2021-06-03 17:13:59 -0400

    moved projects into assets folder

--- script.js
+++ script.js
@@ -31,27 +31,27 @@ const imgThree = document.getElementById("imageThree");
 
 // FUNCTION FOR MOVIES-TO-Z
 function changeImageOneRight() {
-  if (imgOne.src.match("./projects/movies1.png")){
-    imgOne.src="./projects/movies2.png"
+  if (imgOne.src.match("./assets/projects/movies1.png")){
+    imgOne.src="./assets/projects/movies2.png"
     imgOne.alt="A preview of the Movies A-to-Z app displayed on an iPhone 11 6.1 inch display"
-  } else if (imgOne.src.match("./projects/movies2.png")){
-    imgOne.src="./projects/movies3.png"
+  } else if (imgOne.src.match("./assets/projects/movies2.png")){
+    imgOne.src="./assets/projects/movies3.png"
     imgOne.alt="A preview of the Movies A-to-Z app displayed on an iPad 9.7 inch display"
   } else {
-    imgOne.src="./projects/movies1.png"
+    imgOne.src="./assets/projects/movies1.png"
     imgOne.alt="A preview of the Movies A-to-Z app displayed on a Macbook Pro 16 inch display"
   }
 }
 
 function changeImageOneLeft() {
-  if (imgOne.src.match("./projects/movies1.png")){
-    imgOne.src="./projects/movies3.png"
+  if (imgOne.src.match("./assets/projects/movies1.png")){
+    imgOne.src="./assets/projects/movies3.png"
     imgOne.alt="A preview of the Movies A-to-Z app displayed on an iPad 9.7 inch display"
-  } else if (imgOne.src.match("./projects/movies3.png")){
-    imgOne.src="./projects/movies2.png"
+  } else if (imgOne.src.match("./assets/projects/movies3.png")){
+    imgOne.src="./assets/projects/movies2.png"
     imgOne.alt="A preview of the Movies A-to-Z app displayed on an iPhone 11 6.1 inch display"
   } else {
-    imgOne.src="./projects/movies1.png"
+    imgOne.src="./assets/projects/movies1.png"
     imgOne.alt="A preview of the Movies A-to-Z app displayed on a Macbook Pro 16 inch display"
   }
 }
@@ -59,54 +59,54 @@ function changeImageOneLeft() {
 
 // FUNCTION FOR PODCAST PRIORITIZER
 function changeImageTwoRight() {
-  if (imgTwo.src.match("./projects/podcast1.png")){
-    imgTwo.src="./projects/podcast2.png"
+  if (imgTwo.src.match("./assets/projects/podcast1.png")){
+    imgTwo.src="./assets/projects/podcast2.png"
     imgTwo.alt="A preview of the Podcast Prioritizer app displayed on an iPhone 11 6.1 inch display"
-  } else if (imgTwo.src.match("./projects/podcast2.png")){
-    imgTwo.src="./projects/podcast3.png"
+  } else if (imgTwo.src.match("./assets/projects/podcast2.png")){
+    imgTwo.src="./assets/projects/podcast3.png"
     imgTwo.alt="A preview of the Podcast Prioritizer app displayed on an iPad 9.7 inch display"
   } else {
-    imgTwo.src="./projects/podcast1.png"
+    imgTwo.src="./assets/projects/podcast1.png"
     imgTwo.alt="A preview of the Podcast Prioritizer app displayed on a Macbook Pro 16 inch display"
   }
 }
 
 function changeImageTwoLeft() {
-  if (imgTwo.src.match("./projects/podcast1.png")){
-    imgTwo.src="./projects/podcast3.png"
+  if (imgTwo.src.match("./assets/projects/podcast1.png")){
+    imgTwo.src="./assets/projects/podcast3.png"
     imgTwo.alt="A preview of the Podcast Prioritizer app displayed on an iPad 9.7 inch display"
-  } else if (imgTwo.src.match("./projects/podcast3.png")){
-    imgTwo.src="./projects/podcast2.png"
+  } else if (imgTwo.src.match("./assets/projects/podcast3.png")){
+    imgTwo.src="./assets/projects/podcast2.png"
     imgTwo.alt="A preview of the Podcast Prioritizer app displayed on an iPhone 11 6.1 inch display"
   } else {
-    imgTwo.src="./projects/podcast1.png"
+    imgTwo.src="./assets/projects/podcast1.png"
     imgTwo.alt="A preview of the Podcast Prioritizer app displayed on a Macbook Pro 16 inch display"
   }
 }
 
 // FUNCTION FOR SURVIVING BOOTCAMP
 function changeImageThreeRight() {
-  if (imgThree.src.match("./projects/surviving1.png")){
-    imgThree.src="./projects/surviving2.png"
+  if (imgThree.src.match("./assets/projects/surviving1.png")){
+    imgThree.src="./assets/projects/surviving2.png"
     imgThree.alt="A preview of the Surviving Bootcamp app displayed on an iPhone 11 6.1 inch display"
-  } else if (imgThree.src.match("./projects/surviving2.png")){
-    imgThree.src="./projects/surviving3.png"
+  } else if (imgThree.src.match("./assets/projects/surviving2.png")){
+    imgThree.src="./assets/projects/surviving3.png"
     imgThree.alt="A preview of the Surviving Bootcamp app displayed on an iPad 9.7 inch display"
   } else {
-    imgThree.src="./projects/surviving1.png"
+    imgThree.src="./assets/projects/surviving1.png"
     imgThree.alt="A preview of the Surviving Bootcamp app displayed on a Macbook Pro 16 inch display"
   }
 }
 
 function changeImageThreeLeft() {
-  if (imgThree.src.match("./projects/surviving1.png")){
-    imgThree.src="./projects/surviving3.png"
+  if (imgThree.src.match("./assets/projects/surviving1.png")){
+    imgThree.src="./assets/projects/surviving3.png"
     imgThree.alt="A preview of the Surviving Bootcamp app displayed on an iPad 9.7 inch display"
-  } else if (imgThree.src.match("./projects/surviving3.png")){
-    imgThree.src="./projects/surviving2.png"
+  } else if (imgThree.src.match("./assets/projects/surviving3.png")){
+    imgThree.src="./assets/projects/surviving2.png"
     imgThree.alt="A preview of the Surviving Bootcamp app displayed on an iPhone 11 6.1 inch display"
   } else {
-    imgThree.src="./projects/surviving1.png"
+    imgThree.src="./assets/projects/surviving1.png"
     imgThree.alt="A preview of the Surviving Bootcamp app displayed on a Macbook Pro 16 inch display"
   }
 }
